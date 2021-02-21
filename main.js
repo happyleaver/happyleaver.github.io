efbb40de4cf7d161b1441f57bb626ef304b1015b
leavehomehappily <lijadi5288@combcub.com>
2021-02-21 22:23:53 +0800

    apple6

--- main.js
+++ main.js
@@ -102,8 +102,8 @@ async function getcode() {
     while (video.readyState != video.HAVE_ENOUGH_DATA) await wait_frame_out();
     let vh = canvas.height = video.videoHeight;
     let vw = canvas.width = video.videoWidth;
-    let ch = document.body.clientWidth / vw * vh;
-    let ct = (document.body.clientHeight - ch) / 2;
+    let ch = window.innerWidth / vw * vh;
+    let ct = (window.innerHeight - ch) / 2;
     canvas.style.top = ct + 'px';
     canvas.style.height = ch + 'px';
     canvas.style.width = '100vw';
